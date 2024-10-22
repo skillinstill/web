adbf79ab06ed0c306677f28109904e658c487541
hari <devignx@gmail.com>
2024-10-22 10:11:32 +0530

    seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,17 +4,20 @@ import Footer from "./components/footer";
 import "./globals.css";
 
 export const metadata = {
-    title: "SkillInstill",
-    description: "Training • Consulting • Development",
+    title: "SkillInstill | Training, Consulting, Development, and Technology Hiring",
+    description:
+        "Training, Consulting, Development, and Technology Hiring. As the fastest-growing IT Services company in India, we take pride in providing an unparalleled range of services all under one roof",
     metadataBase: new URL("https://skillinstill.com/"),
     openGraph: {
         title: "SkillInstill",
-        description: "Training • Consulting • Development",
+        description:
+            "Training, Consulting, Development, and Technology Hiring. As the fastest-growing IT Services company in India, we take pride in providing an unparalleled range of services all under one roof",
         url: "https://skillinstill.com/",
-        siteName: "SkillInstill",
+        siteName:
+            "SkillInstill | Training, Consulting, Development, and Technology Hiring",
         images: [
             {
-                url: "https://skillinstill.com/icon.png",
+                url: "https://skillinstill.com/og.png",
             },
         ],
         locale: "en_US",
@@ -22,10 +25,11 @@ export const metadata = {
     },
     twitter: {
         card: "summary_large_image",
-        title: "SkillInstill",
-        description: "Training • Consulting • Development",
+        title: "SkillInstill | Training, Consulting, Development, and Technology Hiring",
+        description:
+            "Training, Consulting, Development, and Technology Hiring. As the fastest-growing IT Services company in India, we take pride in providing an unparalleled range of services all under one roof",
         // creator: "@haripras_d",
-        image: "https://skillinstill.com/icon.png",
+        image: "https://skillinstill.com/og.png",
     },
 };
 
@@ -34,6 +38,10 @@ const figtree = Figtree({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
     return (
         <html data-theme="light" lang="en">
+            <head>
+                <link rel="icon" href="/favicon.png" />
+                <link rel="canonical" href="https://skillinstill.com/" />
+            </head>
             <body
                 className={` ${figtree.className} flex flex-col min-h-100dvh antialiased`}
             >
